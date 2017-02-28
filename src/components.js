45c9703b40e0cdcc12a636247b4cc558d8ebbe26
bodhi <bodhi@lianj.com>
2017-02-28 18:45:14 +0800

    update path

--- src/components.js
+++ src/components.js
@@ -1,5 +1,5 @@
 
-      const component0 = require('/Users/tofishes/Documents/git/me/a-rule/components/hello.vue');
+      const component0 = require('/Users/tofishes/Documents/git/me/a-rule/src/components/hello.vue');
       Vue.component('hello', component0);
-      const component1 = require('/Users/tofishes/Documents/git/me/a-rule/components/m-header.vue');
+      const component1 = require('/Users/tofishes/Documents/git/me/a-rule/src/components/m-header.vue');
       Vue.component('m-header', component1);
