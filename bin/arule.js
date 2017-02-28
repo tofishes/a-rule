b534107763fc41003af71a91be2a7fcd53dfefbe
bodhi <bodhi@lianj.com>
2017-02-28 16:46:07 +0800

    update arule commond

--- bin/arule.js
+++ bin/arule.js
@@ -7,13 +7,14 @@ require('../gulpfile');
 program.on('--help', () => {
   log.warn('  Usage:');
   log.warn('  $', 'arule dev', '测试环境编译');
-  log.warn('  $', 'arule pord', '生产环境编译');
+  log.warn('  $', 'arule prod', '生产环境编译');
 });
 
 program.parse(process.argv);
 const task = program.args[0];
+const right = ~['dev', 'prod'].indexOf(task); // eslint-disable-line
 
-if (task) {
+if (right) {
   gulp.start(task);
 } else {
   program.help();
